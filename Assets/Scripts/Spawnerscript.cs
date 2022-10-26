46c85d46171cd3fb1bb37cc3512bb88287ca9a9b
20-Royston <coldroy1235@gmail.com>
2022-10-26 10:34:03 +0800

    update spawner

--- Assets/Scripts/Spawnerscript.cs
+++ Assets/Scripts/Spawnerscript.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Spawnerscript : MonoBehaviour
 {
-    public GameObject SpawnObject;
+    public GameObject[] SpawnObject;
+    
     float PositionY;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObjects", 1, 1);
+        
     }
 
     // Update is called once per frame
@@ -25,6 +27,8 @@ public class Spawnerscript : MonoBehaviour
     {
         PositionY = Random.Range(4, -4f);
         this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
-        Instantiate(SpawnObject, transform.position, transform.rotation);
+        Instantiate(SpawnObject[Random.Range(0, SpawnObject.Length)], transform.position, transform.rotation);
     }
+
+    
 }
