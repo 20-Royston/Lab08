b4d6dfad796326e91f4f13fb0852616534e680ac
20-Royston <coldroy1235@gmail.com>
2022-10-26 10:56:44 +0800

    summary

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float speed;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +19,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y >= 4 || transform.position.y <= -4)
+        {
+            transform.position = new Vector3(-6, 0, 0);
+        }
 
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
 
-      
+        
+    }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("Lose");
+        }
     }
 }
